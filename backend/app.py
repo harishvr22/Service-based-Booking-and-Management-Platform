002a293d95ad717c8b35786a5d59ef2597dde035
harishvr22 <harishviji302@gmail.com>
2026-03-10 15:36:00 +0530

    adding App.py backend

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,21 @@
 from flask import Flask
 from flask_cors import CORS
 
+from routes.auth import auth_bp
+from routes.services import services_bp
+from routes.bookings import booking_bp
+
 app = Flask(__name__)
 CORS(app)
 
+# Register routes
+app.register_blueprint(auth_bp)
+app.register_blueprint(services_bp)
+app.register_blueprint(booking_bp)
+
 @app.route("/")
 def home():
-    return "Backend Running Successfully"
+    return "Apartment Service Backend Running"
 
 if __name__ == "__main__":
     app.run(debug=True)
