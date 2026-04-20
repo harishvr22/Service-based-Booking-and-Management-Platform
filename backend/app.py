5536cfdb618407fe564d8e29e970031ccb626f21
harishvr22 <harishviji302@gmail.com>
2026-04-20 20:37:54 +0530

    removing unused pages

--- backend/app.py
+++ backend/app.py
@@ -1,21 +1,25 @@
 from flask import Flask
 from flask_cors import CORS
+from socketio_instance import socketio
 
 from routes.auth import auth_bp
 from routes.services import services_bp
 from routes.bookings import booking_bp
+from routes.notifications import notifications_bp
 
 app = Flask(__name__)
 CORS(app)
+socketio.init_app(app, cors_allowed_origins="*")
 
 # Register routes
 app.register_blueprint(auth_bp)
 app.register_blueprint(services_bp)
 app.register_blueprint(booking_bp)
+app.register_blueprint(notifications_bp)
 
 @app.route("/")
 def home():
     return "Apartment Service Backend Running"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    socketio.run(app, debug=True)
